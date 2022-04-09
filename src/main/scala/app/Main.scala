6bb3195a624e1f596f0651b78b8cd09e62f7ad86
fiatjaf <fiatjaf@gmail.com>
2022-04-09 14:22:42 -0300

    asynchronous experiment with stdin and castor actors.

--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -1,11 +1,15 @@
 package app
 
+import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.Future
+import scala.scalanative.loop.Timer
+import scala.scalanative.loop.Poll
 import castor.Context
-
-import app.codecs.CommonCodecs
 
 object Main {
   def main(args: Array[String]): Unit = {
+    println("start")
+
     val l1 = new StringLogger("foo")(castor.Context.Simple.global)
     val l2 = new StringLogger("xiq")(castor.Context.Simple.global)
     val i1 = new IntLogger("bar")(castor.Context.Simple.global)
@@ -16,14 +20,28 @@ object Main {
     l1.send("hello2")
     l2.send("hello2")
 
-    println(CommonCodecs.bool8)
+    println("before timer")
+    Timer.timeout(FiniteDuration(5, "seconds")) { () =>
+      println("will read stdin")
+
+      Poll(0).startRead { _ =>
+        val line = scala.io.StdIn.readLine().trim
+        println(s"line: $line")
+        if (line.size > 0) {
+          l1.send(line)
+        }
+      }
+    }
   }
 }
 
 class StringLogger(name: String)(implicit ac: castor.Context)
     extends castor.SimpleActor[String] {
   def run(s: String): Unit = {
-    println(s"$name: ${s}")
+    println(s"$name immediately: ${s}")
+    Timer.timeout(FiniteDuration(2, "seconds")) { () =>
+      println(s"$name after 2 seconds: ${s}")
+    }
   }
 }
 
